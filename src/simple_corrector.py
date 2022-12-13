031924a9e36b0a5072588d00e549b5e2aaf8c9ec
wahyu9 <wahyu@valiance.ai>
2022-12-13 17:24:48 +0700

    Sample of Sundanese corpus

--- src/simple_corrector.py
+++ src/simple_corrector.py
@@ -41,8 +41,22 @@ def edits2(word):
     "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
+# Create a function allowing the app to receive Sentences as input
+def correct_sentence(sentence):
+    # Split the sentence into words
+    words = re.findall(r'\b[\w\']+\b', sentence)
+    
+    # Correct the spelling of each word
+    corrected_words = []
+    for word in words:
+        corrected_word = correction(word)
+        corrected_words.append(corrected_word)
+    
+    # Join the corrected words into a sentence
+    corrected_sentence = ' '.join(corrected_words)
+    
+    return corrected_sentence# Create the app
 
 if __name__ == "__main__":
-    PATH = "data/big.txt"
+    PATH = "data/Sundanese.txt"
     WORDS = words_loader(PATH)
-    print(correction("barcket",WORDS))
