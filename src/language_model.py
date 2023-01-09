1449d2ae61b0c4ab26571c84323402f072d9498d
Ghifari Adam Faza <fazaghifari@Ghifaris-MacBook-Pro.local>
2023-01-09 09:07:49 +0700

    finish script for training stupidbackoff language model

--- src/language_model.py
+++ src/language_model.py
@@ -6,12 +6,34 @@ import time
 import nltk
 
 def text_loader(PATH):
+    """
+    Function for loading and converting blob of text into nested list (NLTK format)
+
+    Args:
+        PATH (str): The path of the blob of text
+
+    Returns:
+        tokens_sentences (list): Tokenized sentences
+    """
+
     input_text = open(PATH).read()
     tokenizer_words = TweetTokenizer()
     tokens_sentences = [[word.lower() for word in tokenizer_words.tokenize(t) if word.isalpha()] for t in sent_tokenize(input_text)]
     return tokens_sentences
 
 def text_padder(tokenized_text, n=2):
+    """
+    Pad text with special character
+
+    Args:
+        tokenized_text (list): Tokenized text from `text_loader` function in this script
+        n (int, optional): number of n_gram. Defaults to 2.
+
+    Returns:
+        train_data (generator): data for n_gram model training
+        padded_sents (generator): vocabulary for n_gram model training
+    """
+
     train_data, padded_sents = padded_everygram_pipeline(n, tokenized_text)
     return train_data, padded_sents
 
@@ -30,7 +52,5 @@ def main_lm(PATH, n):
 if __name__ == "__main__":
 
     PATH = "data/big.txt"
-    t0 = time.time()
     model = main_lm(PATH, 3)
-    print(f"time needed = {time.time() - t0} seconds")
     joblib.dump(model, "data/lm_english.pkl")
